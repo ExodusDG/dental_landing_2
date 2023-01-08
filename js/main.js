5851513e726131bb5cc861ddf130bb048104e511
Mykola Perebenesiuk <exodusdevelop@gmail.com>
2023-01-08 13:44:35 +0200

    upd

--- js/main.js
+++ js/main.js
@@ -41,3 +41,37 @@ updateTime()
 setInterval(() => {
     updateTime()
 }, 1000);
+
+
+var duration = moment.duration({
+    'minutes': 5,
+    'seconds': 00
+
+});
+
+var timestamp = new Date(0, 0, 0, 2, 10, 30);
+var interval = 1;
+var timer = setInterval(function () {
+    timestamp = new Date(timestamp.getTime() + interval * 1000);
+
+    duration = moment.duration(duration.asSeconds() - interval, 'seconds');
+    var min = duration.minutes();
+    var sec = duration.seconds();
+    var hours = duration.hours();
+
+    sec -= 1;
+    if (min < 0) return clearInterval(timer);
+    if (min < 10 && min.length != 2) min = '0' + min;
+    if (sec < 0 && min != 0) {
+        min -= 1;
+        sec = 59;
+    } else if (sec < 10 && length.sec != 2) sec = '0' + sec;
+
+    $('#hours').text(hours)
+    $('#minutes').text(min)
+    $('#seconds').text(sec)
+    if (min == 0 && sec == 0)
+        clearInterval(timer);
+
+
+}, 1000);
